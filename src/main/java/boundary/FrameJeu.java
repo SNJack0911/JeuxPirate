e225b097476cad287db639db27a0055f88e64cfe
NGUYEN DUC HAI <duc-hai.nguyen@univ-tlse3.fr>
2025-04-08 16:26:42 +0200

    Update hirearchy

--- src/main/java/boundary/FrameJeu.java
+++ src/main/java/boundary/FrameJeu.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.ilu4.jeuxpirate.boundary;
+package boundary;
 
 import java.io.File;
 import java.io.IOException;
-import com.ilu4.jeuxpirate.boundary.components.*;
+import boundary.components.*;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
